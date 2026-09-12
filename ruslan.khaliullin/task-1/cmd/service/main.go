faff303fb7c8beaa179281eaf188a2bcb8137851
knopkloop <hruslan2007@gmail.com>
2026-09-13 02:57:56 +0300

    [TASK-1] Add input data processing

--- ruslan.khaliullin/task-1/cmd/service/main.go
+++ ruslan.khaliullin/task-1/cmd/service/main.go
@@ -1,1 +1,26 @@
 package main
+
+import "fmt"
+
+func main() {
+	var n1, n2 int
+	var op string
+
+	_, err := fmt.Scan(&n1)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	_, err = fmt.Scan(&n2)
+	if err != nil {
+		fmt.Println("Ivalid second operand")
+		return
+	}
+
+	_, err = fmt.Scan(&op)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+}
