8fa3ade90dfa327d86a271fc3d35a1528ed12852
knopkloop <hruslan2007@gmail.com>
2026-09-13 03:13:40 +0300

    [TASK-1] Add switch-case for operations

--- ruslan.khaliullin/task-1/cmd/service/main.go
+++ ruslan.khaliullin/task-1/cmd/service/main.go
@@ -14,7 +14,7 @@ func main() {
 
 	_, err = fmt.Scan(&n2)
 	if err != nil {
-		fmt.Println("Ivalid second operand")
+		fmt.Println("Invalid second operand")
 		return
 	}
 
@@ -22,5 +22,22 @@ func main() {
 	if err != nil {
 		fmt.Println("Invalid operation")
 		return
+	}
+
+	switch op {
+	case "+":
+		fmt.Println(n1 + n2)
+	case "-":
+		fmt.Println(n1 - n2)
+	case "*":
+		fmt.Println(n1 * n2)
+	case "/":
+		if n2 == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		fmt.Println(float64(n1) / float64(n2))
+	default:
+		fmt.Println("Invalid operation")
 	}
 }
